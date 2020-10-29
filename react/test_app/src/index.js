c62dbca0acb94a9e0704860d1d45ebf5f2a997ad
quietsos <shohaniu8@gmail.com>
2020-10-30 04:47:47 +0600

    React expression

---
+++ react/test_app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+
+  <>
+  <h1>This is a demo test examaple.....</h1>
+  <h3>List of favourit show</h3>
+  <ol>
+    <li> Avengers </li>
+    <li> Dark</li>
+  </ol>
+  </>
+  ,
+  document.getElementById('root')
+);
