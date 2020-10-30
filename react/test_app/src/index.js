12cb4d9b3f256b1c8cf417fddca82eee6e22af6c
quietsos <shohaniu8@gmail.com>
2020-10-30 19:00:44 +0600

    image handling using template literal

--- react/test_app/src/index.js
+++ react/test_app/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const fname = "Md";
+const lname = "shohanuzzama";
+var d = new Date();
 
 ReactDOM.render(
-
   <>
   <h1>This is a demo test examaple.....</h1>
   <h3>List of favourit show</h3>
@@ -11,7 +13,11 @@ ReactDOM.render(
     <li> Avengers </li>
     <li> Dark</li>
   </ol>
+
+  <h1>{`My name is ${fname} ${lname} `}</h1>
+  <p>todays Date is </p>
+
   </>
   ,
-  document.getElementById('root')
+  document.getElementById('root').innerHTML = d 
 );
