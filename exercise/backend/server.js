2fea0103fa9a47aa24e83a2ec0d2a5d363d613da
Md Shohanuzzaman <shohaniu8@gmail.com>
2021-06-06 22:10:54 +0600

    react

---
+++ exercise/backend/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middole ware 
+app.use(cors());
+app.use(express.json());
+
+ 
+const uri = process.env.mongo_url;
+mongoose.connect(uri,{ useUnifiedTopology:true,useNewUrlParser:true,useCreateIndex:true});
+
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log("MongoDB database connection establish successfully.");
+})
+
+
+
+const exerciseRouter = require('./routes/exercises');
+const userRouter = require('./routes/users');
+
+
+
+
+app.use('/exercises',exerciseRouter);
+app.use('/users',userRouter);
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
+
+
